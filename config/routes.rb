d6edb40bb18d162069300ec456407c5338939d3b
PhiNguyen <phinguyenphuoc@gmail.com>
2019-02-14 14:43:19 +0700

    update chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    root "home_controller#home"
+  end
+  get "/about" => "home_controller#about"
+  get "/contact" => "home_controller#contact"
+
 end
